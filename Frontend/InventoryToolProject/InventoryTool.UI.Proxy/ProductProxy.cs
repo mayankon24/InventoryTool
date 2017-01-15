21cd79cd7f268c17739a42365b2c6db74f6ee797
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-01-15 15:27:29 +0530

    Add newget packages

---
+++ Frontend/InventoryToolProject/InventoryTool.UI.Proxy/ProductProxy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryTool.UI.Proxy
+{
+    public class ProductProxy
+    {
+        private static ProductProxy instance;
+        private static readonly object padlock = new object();
+
+        private ProductProxy()
+        {
+        }
+
+        public static ProductProxy Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new ProductProxy();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+
+        public void AddUpdateProducts(string apiUri, string requestUri)
+        {
+            
+        }
+
+    }
+}
---
+++ Frontend/InventoryToolProject/InventoryTool.UI.Proxy/ProxyHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+
+namespace InventoryTool.UI.Proxy
+{
+
+    class ProxyHelper
+    { /// <summary>
+      /// The Internet media type or Content type
+      /// </summary>
+        private const string MediaType = "application/json";
+
+        public static string GetResponseFromApi(string apiUri, string requestUri)
+        {
+            string result;
+
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                UseDefaultCredentials = true
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(apiUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaType));
+
+                using (HttpResponseMessage response = client.GetAsync(requestUri).Result)
+                {
+                    result = GetResultStringObject(response);
+                }
+            }
+
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Gets the response from API.
+        /// </summary>
+        /// <typeparam name="T">Type is the object type in which result will return</typeparam>
+        /// <param name="apiUri">The API URI.</param>
+        /// <param name="requestUri">The request URI.</param>
+        /// <param name="requestData" />
+        /// <returns>Result will return in the form of generic type</returns>
+        public static string PostRequestToApi<T>(string apiUri, string requestUri, T requestData)
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                UseDefaultCredentials = true
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                string postData;
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(requestData.GetType());
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    serializer.WriteObject(ms, requestData);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    using (StreamReader sr = new StreamReader(ms))
+                    {
+                        postData = sr.ReadToEnd();
+                    }
+                }
+
+                client.BaseAddress = new Uri(apiUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaType));
+                using (var response = client.PostAsync(requestUri, new StringContent(postData, Encoding.UTF8, "application/json")).Result)
+                {
+                    return GetResultStringObject(response);
+                }
+            }
+        }
+
+        private static string GetResultStringObject(HttpResponseMessage response)
+        {
+            string result;
+            using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+            {
+                var reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+            }
+
+            return result;
+        }
+    }
+}
