0cf9505a5ffca61f1dc91edaff8825c41c13b309
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-01-23 00:24:43 +0530

    update database

---
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Stored Procedures/InsertErrorLog.sql
@@ -0,0 +1,23 @@
+﻿
+
+create PROCEDURE [dbo].[InsertErrorLog] (
+                @Window_Id INT  
+                ,@Source NVARCHAR(100)
+                ,@Source_Detail NVARCHAR(1000)
+                ,@Error_description nvarchar(max)         
+                )
+AS
+
+
+INSERT INTO [dbo].[ErrorLog]
+           ([Window_Id]
+           ,[Source]
+           ,[Source_Detail]
+           ,[Error_description]
+           ,[Inserted_Date])
+     VALUES
+           (@Window_Id
+           ,@Source
+           ,@Source_Detail
+           ,@Error_description
+           ,getutcdate())
---
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Stored Procedures/UpdateProduct.sql
@@ -0,0 +1,92 @@
+﻿
+
+create PROCEDURE [dbo].[UpdateProduct] (
+					@Product_Id INT
+					,@Product_Code nvarchar(100)
+					,@Product_Name nvarchar(100)
+					,@Manufacturing_Days int
+					,@Description nvarchar(1000)
+					,@ModifiedBy NVARCHAR(100)
+					,@return_Status INT OUTPUT
+                )
+AS
+BEGIN TRY
+                BEGIN TRANSACTION
+
+                
+                if exists(select 1 from [M_Product] 
+                          where Product_Code = @Product_Code                                             
+                                                  and isactive = 1 
+                                                  and @Product_Id <= 0)
+                                                  SET @return_status = -2
+
+                else if exists (select 1 from [M_Product]  
+                          where Product_Code = @Product_Code                                                               
+                                                  and isactive = 1 
+                                                  and @Product_Id > 0
+                                                  and Product_Id <> @Product_Id)
+
+                SET @return_status = -2
+
+                
+                ELSE IF (@Product_Id <= 0)
+                BEGIN
+                                
+						INSERT INTO [dbo].[M_Product]
+								   ([Product_Code]
+								   ,[Product_Name]
+								   ,[Manufacturing_Days]
+								   ,[Description]
+								   ,[CreatedBy]
+								   ,[CreatedUTCDate]
+								   ,[LastModifiedBy]
+								   ,[LastModifiedUTCDate]
+								   ,[IsActive])
+							 VALUES
+								   (@Product_Code
+								   ,@Product_Name
+								   ,@Manufacturing_Days
+								   ,@Description
+								   ,@ModifiedBy
+								   ,getutcdate()
+								   ,null
+								   ,null
+								   ,1)
+
+                                SET @return_status = 1
+
+                                SELECT SCOPE_IdENTITY() AS Id
+                END
+                ELSE
+                                begin
+                                UPDATE [dbo].[M_Product]
+								SET [Product_Code] = @Product_Code
+									,[Product_Name] = @Product_Name
+									,[Manufacturing_Days] = @Manufacturing_Days
+									,[Description] = @Description
+									,[LastModifiedBy] = @ModifiedBy
+									,[LastModifiedUTCDate] = getutcdate()
+									
+								WHERE Product_Id = @Product_Id
+
+                SET @return_status = 2
+                end
+                COMMIT TRANSACTION
+END TRY
+
+BEGIN CATCH
+                ROLLBACK TRANSACTION
+
+                SET @return_status = - 1
+
+                declare @error_message nvarchar(max)
+                SELECT @error_message = 
+                    'ERROR_NUMBER=' +cast(isnull(ERROR_NUMBER(), '') as nvarchar(10))+ ' || '+
+                                'ERROR_SEVERITY=' +cast(isnull(ERROR_SEVERITY(), '')as nvarchar(10))+ ' || '+
+                                'ERROR_STATE=' +cast(isnull(ERROR_STATE(), '')as nvarchar(10))+ ' || '+
+                                'ERROR_PROCEDURE=' +isnull(ERROR_PROCEDURE(), '')+ ' || '+
+                                'ERROR_LINE=' +cast(isnull(ERROR_LINE(), '')as nvarchar(10))+ ' || '+
+                                'ERROR_MESSAGE=' +isnull(ERROR_MESSAGE(), '')+ ' || '                                
+                exec InsertErrorLog null, 'Database', 'UpdateClass', @error_message
+
+END CATCH
