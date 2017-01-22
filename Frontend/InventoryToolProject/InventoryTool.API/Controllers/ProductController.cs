7577a4b18b05d78988f4a35746aee59f1fd6e552
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-01-22 23:51:01 +0530

    Update entity for product

--- Frontend/InventoryToolProject/InventoryTool.API/Controllers/ProductController.cs
+++ Frontend/InventoryToolProject/InventoryTool.API/Controllers/ProductController.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Data.Entity.Core.Objects;
+using System.Web.Http;
+using InventoryTool.Model;
+using System.Data.Objects;
 
 namespace InventoryTool.API.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : ApiController
     {
-        // GET: Product
-        public ActionResult Index()
+        [Route("UpdateGroup")]
+        [HttpPost]
+        public IHttpActionResult UpdateGroup(M_Product product)
         {
-            return View();
+            var return_Status = new System.Data.Objects.ObjectParameter("return_Status", typeof(int));
+            using (InventoryToolDBEntities entity = new InventoryToolDBEntities())
+            {
+                entity.UpdateProduct(product.Product_Id, product.Product_Code, product.Product_Name, product.Manufacturing_Days, product.Description, product.LastModifiedBy, return_Status);
+                return Ok(return_Status.Value);
+
+
+            }
         }
     }
 }
