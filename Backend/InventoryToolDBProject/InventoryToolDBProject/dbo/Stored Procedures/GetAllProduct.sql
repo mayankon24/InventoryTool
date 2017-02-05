ae87030b8637a31508348d9e0b961e2d655d908c
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-02-05 19:01:17 +0530

    add procedure

---
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Stored Procedures/GetAllProduct.sql
@@ -0,0 +1,12 @@
+﻿
+CREATE procedure [dbo].[GetAllProduct]
+
+as 
+SELECT [Product_Id]
+      ,[Product_Code]
+      ,[Product_Name]
+      ,[Manufacturing_Days]
+      ,[Description]
+  FROM [M_Product]
+  where IsActive = 1
+  order by Product_Name
