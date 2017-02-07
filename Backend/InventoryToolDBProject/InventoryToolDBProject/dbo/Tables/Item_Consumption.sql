778aecc46dfe4df0eda7f38cc4734d73e29de167
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-02-07 17:29:48 +0530

    remove row material item

--- Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Tables/Item_Consumption.sql
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Tables/Item_Consumption.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Item_Consumption] (
     [Item_Consumption_Id]  INT             IDENTITY (1, 1) NOT NULL,
-    [Row_Material_Item_Id] INT             NULL,
+    [Part_Id] INT             NULL,
     [Machine_Process_Id]   INT             NULL,
     [Quantity]             DECIMAL (18, 2) NULL,
     [Date]                 DATETIME        NULL,
@@ -8,6 +8,6 @@
     [IsActive]             BIT             NULL,
     CONSTRAINT [PK_Item_Consumption] PRIMARY KEY CLUSTERED ([Item_Consumption_Id] ASC),
     CONSTRAINT [FK_Item_Consumption_Machine_Process] FOREIGN KEY ([Machine_Process_Id]) REFERENCES [dbo].[Machine_Process] ([Machine_Process_Id]),
-    CONSTRAINT [FK_Item_Consumption_Row_Material_Item] FOREIGN KEY ([Row_Material_Item_Id]) REFERENCES [dbo].[Row_Material_Item] ([Row_Material_Item_Id])
+    
 );
 
--- Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Tables/M_PURCHASES_ORDER_DETAIL.sql
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Tables/M_PURCHASES_ORDER_DETAIL.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[M_PURCHASES_ORDER_DETAIL] (
     [Purchase_Order_Detail_Id] INT             IDENTITY (1, 1) NOT NULL,
     [Purchases_Order_Id]       INT             NULL,
-    [Row_Material_Item_Id]     INT             NULL,
+    [Part_Id]     INT             NULL,
     [Notes]                    NVARCHAR (1000) NULL,
     [Quantity]                 FLOAT (53)      NULL,
     [Item_Rate]                FLOAT (53)      NULL,
@@ -10,6 +10,6 @@
     [future2]                  NCHAR (10)      NULL,
     CONSTRAINT [PK_M_PURCHASES_ORDER_DETAIL] PRIMARY KEY CLUSTERED ([Purchase_Order_Detail_Id] ASC),
     CONSTRAINT [FK_M_PURCHASES_ORDER_DETAIL_M_PURCHASES_ORDER] FOREIGN KEY ([Purchases_Order_Id]) REFERENCES [dbo].[M_PURCHASES_ORDER] ([Purchases_Order_Id]),
-    CONSTRAINT [FK_M_PURCHASES_ORDER_DETAIL_Row_Material_Item] FOREIGN KEY ([Row_Material_Item_Id]) REFERENCES [dbo].[Row_Material_Item] ([Row_Material_Item_Id])
+    
 );
 
