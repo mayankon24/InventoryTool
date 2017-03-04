bb78d91459c1f9d0353c9a86f8a8567801e8c440
mayankon24 <mayankaggarwal.cse@gmail.com>
2017-03-04 16:33:33 +0530

    Update database

--- Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Stored Procedures/GetAllPart.sql
+++ Backend/InventoryToolDBProject/InventoryToolDBProject/dbo/Stored Procedures/GetAllPart.sql
@@ -20,11 +20,12 @@ SELECT [Part_Id]
 	  ,Cl.Criticality_Id
 	  ,Cl.Criticality_Name
   FROM M_Part PA 
-       inner join LK_Material M on M.Material_Id = PA.Material_Id and M.IsActive = 1
-	   inner join LK_Unit U on U.Unit_Id = PA.Unit_Id and U.IsActive = 1
-	   inner join Lk_Category C on C.Category_Id = PA.Category_Id and C.IsActive = 1
-	   inner join Lk_Color CO on CO.Color_Id = PA.Color_Id and CO.IsActive = 1
-	   inner join LK_Outsource_Type O on O.Outsource_Type_Id = PA.Outsource_Type_Id and O.IsActive = 1
-	   inner join LK_Part_Type P on P.Part_Type_Id = PA.Part_Type_Id and P.IsActive = 1
-	   inner join LK_Criticality Cl on Cl.Criticality_Id = PA.Criticality_Id and Cl.IsActive = 1
+       Left join LK_Material M on M.Material_Id = PA.Material_Id and M.IsActive = 1
+	   Left join LK_Unit U on U.Unit_Id = PA.Unit_Id and U.IsActive = 1
+	   Left join Lk_Category C on C.Category_Id = PA.Category_Id and C.IsActive = 1
+	   Left join Lk_Color CO on CO.Color_Id = PA.Color_Id and CO.IsActive = 1
+	   Left join LK_Outsource_Type O on O.Outsource_Type_Id = PA.Outsource_Type_Id and O.IsActive = 1
+	   Left join LK_Part_Type P on P.Part_Type_Id = PA.Part_Type_Id and P.IsActive = 1
+	   Left join LK_Criticality Cl on Cl.Criticality_Id = PA.Criticality_Id and Cl.IsActive = 1
+ where PA.IsActive = 1
   order by Part_Name
